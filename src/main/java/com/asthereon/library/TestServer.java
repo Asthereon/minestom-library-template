004f1f0406bd9b29670afe560a5f142c517606c8
Asthereon <asthereon@gmail.com>
2021-04-18 16:04:44 -0400

    Initial commit

---
+++ src/main/java/com/asthereon/library/TestServer.java
@@ -0,0 +1,10 @@
+package com.asthereon.library;
+
+import com.asthereon.asthcore.AsthCore;
+
+public class TestServer {
+
+    public static void main(String[] args) {
+        AsthCore.createTestServer();
+    }
+}
---
+++ start.bat
@@ -0,0 +1,2 @@
+java -Xms8G -Xmx8G -jar server-1.0-all.jar
+PAUSE
